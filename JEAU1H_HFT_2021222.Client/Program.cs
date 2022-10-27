a285c5b419b6b65f8c6b20604f99bc429687ac86
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2022-10-27 16:51:11 +0200

    Full Menu created

--- JEAU1H_HFT_2021222.Client/Program.cs
+++ JEAU1H_HFT_2021222.Client/Program.cs
@@ -12,8 +12,8 @@ namespace JEAU1H_HFT_2021222.Client
     class Program
     {
         static GameLogic gamelogic;
-        static MinRequirementsLogic minreqlogic;
-        static StudioLogic studreqlogic;
+        static MinRequirementsLogic minlogic;
+        static StudioLogic studlogic;
        
         static void Main(string[] args)
         {
@@ -22,8 +22,8 @@ namespace JEAU1H_HFT_2021222.Client
             var minrepo = new MinRequirementsRepository(ctx);
             var studrepo = new StudioRepository(ctx);
             gamelogic = new GameLogic(gamerepo, studrepo, minrepo);
-            minreqlogic = new MinRequirementsLogic(minrepo);
-            studreqlogic = new StudioLogic(studrepo);
+            minlogic = new MinRequirementsLogic(minrepo);
+            studlogic = new StudioLogic(studrepo);
 
             /*
            IEnumerable<GameLogic.GamewithMinreq> GamesWithRequirements();
@@ -45,7 +45,7 @@ namespace JEAU1H_HFT_2021222.Client
                 .Add("Exit", ConsoleMenu.Close);
 
 
-            //Games munu
+            //Games menu
             var gamessubmenu = new ConsoleMenu(args, level: 1)
             .Add("Delete", () => Delete("Games"))
             .Add("Create", () => Create("Games"))
@@ -54,13 +54,27 @@ namespace JEAU1H_HFT_2021222.Client
             .Add("Update",() => Overwrite("Games"))
             .Add("Seach by release year", () =>ReleaseYearSearch())
             .Add("Exit", ConsoleMenu.Close);
-            
-
-            //menu
+            //studio menu
+            var studiossubmenu = new ConsoleMenu(args, level: 1)
+            .Add("Delete", () => Delete("Studios"))
+            .Add("Create", () => Create("Studios"))
+            .Add("List", () => List("Studios"))
+            .Add("Search", () => Seach("Studios"))
+            .Add("Update", () => Overwrite("Studios"))
+            .Add("Exit", ConsoleMenu.Close);
+            //req menu
+            var MinRequirementssubmenu = new ConsoleMenu(args, level: 1)
+             .Add("Delete", () => Delete("Requirements"))
+            .Add("Create", () => Create("Requirements"))
+            .Add("List", () => List("Requirements"))
+            .Add("Search", () => Seach("Requirements"))
+            .Add("Update", () => Overwrite("Requirements"))
+            .Add("Exit", ConsoleMenu.Close);
+            //Main menu
             var menu = new ConsoleMenu(args, level: 0)
                 .Add("Games", () => gamessubmenu.Show())
-                //.Add("Studio", () => studiosubmenu.Show())
-                // .Add("MinRequirements", () => MinRequirementssubmenu.Show())
+                .Add("Studio", () => studiossubmenu.Show())
+                .Add("MinRequirements", () => MinRequirementssubmenu.Show())
                 .Add("Exit", ConsoleMenu.Close);
             menu.Show();
             
@@ -81,6 +95,37 @@ namespace JEAU1H_HFT_2021222.Client
                 Console.WriteLine(e.Message);
                     Thread.Sleep(1500);
             }
+                
+            }
+            if (name == "Studios")
+            {
+                Console.WriteLine("ID: ");
+                var id = int.Parse(Console.ReadLine());
+                try
+                {
+                    studlogic.Delete(id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Thread.Sleep(1500);
+                }
+
+            }
+            if (name == "Requirements")
+            {
+                Console.WriteLine("ID: ");
+                var id = int.Parse(Console.ReadLine());
+                try
+                {
+                    minlogic.Delete(id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Thread.Sleep(1500);
+                }
+
             }
         }
         static void Create(string name)
@@ -101,6 +146,32 @@ namespace JEAU1H_HFT_2021222.Client
                 gamelogic.Create(g);
               
             }
+            if (name == "Studios")
+            {
+                Console.WriteLine("Add the name of the Studio: ");
+                var nameg = Console.ReadLine();
+                Console.WriteLine("Add the name of the CEO: ");
+                var ceo = Console.ReadLine();
+                
+
+                Studio g = new Studio(nameg, studlogic.ReadAll().Last().StudioID + 1, ceo);
+
+                studlogic.Create(g);
+
+            }
+            if (name == "Requirements")
+            {
+                Console.WriteLine("GPU: ");
+                var gpu = Console.ReadLine();
+                Console.WriteLine("CPU:");
+                var cpu = Console.ReadLine();
+
+
+                MinRequirements g = new MinRequirements(minlogic.ReadAll().Last().ReqId + 1, cpu,gpu);
+
+                minlogic.Create(g);
+
+            }
         }
         static void List(string name)
         {
@@ -109,6 +180,24 @@ namespace JEAU1H_HFT_2021222.Client
                 foreach(var item in gamelogic.ReadAll())
                 {
                     Console.WriteLine($"{item.Name}| {item.Pyear}|RequirementsID: | {item.ReqId}| StudioID: |{item.StudioId} GameID: |{item.GameID}");
+                }
+                Thread.Sleep(2500);
+
+            }
+            if (name == "Studios")
+            {
+                foreach (var item in studlogic.ReadAll())
+                {
+                    Console.WriteLine($"Studio's name: {item.Name} StudioID: {item.StudioID} CEO's name: {item.CEOName}");
+                }
+                Thread.Sleep(2500);
+
+            }
+            if (name == "Requirements")
+            {
+                foreach (var item in minlogic.ReadAll())
+                {
+                    Console.WriteLine($"ID: {item.ReqId} CPU: {item.CPU} GPU: {item.GPU}");
                 }
                 Thread.Sleep(2500);
 
@@ -126,6 +215,38 @@ namespace JEAU1H_HFT_2021222.Client
                     Console.WriteLine($"{item.Name}| {item.Pyear}|RequirementsID: | {item.ReqId}| StudioID: |{item.StudioId} GameID: |{item.GameID}");
                 }
                 catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Thread.Sleep(2500);
+
+            }
+            if (name == "Studios")
+            {
+                Console.WriteLine("Give the ID of the required item: ");
+                var id = int.Parse(Console.ReadLine());
+                try
+                {
+                    var item = studlogic.Read(id);
+                    Console.WriteLine($"Studio's name: {item.Name} StudioID: {item.StudioID} CEO's name: {item.CEOName}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Thread.Sleep(2500);
+
+            }
+            if (name == "Requirements")
+            {
+                Console.WriteLine("Give the ID of the required item: ");
+                var id = int.Parse(Console.ReadLine());
+                try
+                {
+                    var item = minlogic.Read(id);
+                    Console.WriteLine($"ID: {item.ReqId} CPU: {item.CPU} GPU: {item.GPU}");
+                }
+                catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
@@ -154,6 +275,39 @@ namespace JEAU1H_HFT_2021222.Client
                 var item2 = gamelogic.Read(id);
                 Console.WriteLine("New item: ");
                 Console.WriteLine($"{item2.Name}| {item2.Pyear}|RequirementsID: | {item2.ReqId}| StudioID: |{item2.StudioId} GameID: |{item2.GameID}");
+                Thread.Sleep(3500);
+            }
+            if (name == "Studios")
+            {
+                Console.WriteLine("Add the name of the studio: ");
+                var nameg = Console.ReadLine();
+                Console.WriteLine("Add the name of the CEO: ");
+                var ceo = Console.ReadLine();
+                Console.WriteLine("Add the StudioID: ");
+                var studid = int.Parse(Console.ReadLine());
+                var item = studlogic.Read(studid);
+                Console.WriteLine("Old Item:");
+                Console.WriteLine($"Studio's name: {item.Name} CEO's name: {item.CEOName} StudioID: {item.StudioID}");
+                studlogic.Update(new Studio(nameg, studid, ceo));
+                var item2 = studlogic.Read(studid);
+                Console.WriteLine($"Studio's name: {item2.Name} CEO's name: {item2.CEOName} StudioID: {item2.StudioID}");
+                Thread.Sleep(3500);
+            }
+            if (name == "Requirements")
+            {
+                Console.WriteLine("CPU: ");
+                var cpu = Console.ReadLine();
+                Console.WriteLine("GPU:");
+                var gpu = Console.ReadLine();
+                Console.WriteLine("ID: ");
+                var id = int.Parse(Console.ReadLine());
+                var item = minlogic.Read(id);
+                Console.WriteLine("Old Item:");
+                Console.WriteLine($"CPU: {item.CPU} GPU: {item.GPU} ID: {item.ReqId}");
+                minlogic.Update(new MinRequirements(id,cpu,gpu));
+                var item2 = minlogic.Read(id);
+                Console.WriteLine($"CPU: {item.CPU} GPU: {item.GPU} ID: {item.ReqId}");
+                Thread.Sleep(3500);
             }
         }
         static void ReleaseYearSearch()
