689243f9cf943ebb4318b52438bf2151d8004618
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2022-10-27 10:38:47 +0200

    Crud methods for StudioLogic created

--- JEAU1H_HFT_2021222.Logic/StudioLogic.cs
+++ JEAU1H_HFT_2021222.Logic/StudioLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JEAU1H_HFT_2021222.Models;
+using JEAU1H_HFT_2021222.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,36 @@ namespace JEAU1H_HFT_2021222.Logic
 {
     class StudioLogic
     {
+        IRepository<Studio> repo;
+
+        public StudioLogic(IRepository<Studio> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Studio item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Studio Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IEnumerable<Studio> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Studio item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
