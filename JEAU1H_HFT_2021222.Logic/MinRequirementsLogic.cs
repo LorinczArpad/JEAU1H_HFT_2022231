f81e2e51851d5ecfa34c296e15e8e592b6c5fe93
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2022-10-27 10:36:50 +0200

    Crud methods created for MinRequirementsLigoc

--- JEAU1H_HFT_2021222.Logic/MinRequirementsLogic.cs
+++ JEAU1H_HFT_2021222.Logic/MinRequirementsLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JEAU1H_HFT_2021222.Models;
+using JEAU1H_HFT_2021222.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,36 @@ namespace JEAU1H_HFT_2021222.Logic
 {
     class MinRequirementsLogic
     {
+        IRepository<MinRequirements> repo;
+
+        public MinRequirementsLogic(IRepository<MinRequirements> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(MinRequirements item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public MinRequirements Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IEnumerable<MinRequirements> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(MinRequirements item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
