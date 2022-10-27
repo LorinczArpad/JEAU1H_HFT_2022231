13e5757507cded206a5dafd4c2777dea26dfd7c8
lorincz.arpad@stud.uni-obuda.hu <lorincz.arpad@stud.uni-obuda.hu>
2022-10-27 10:37:35 +0200

    StudioLogic created

---
+++ JEAU1H_HFT_2021222.Logic/StudioLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEAU1H_HFT_2021222.Logic
+{
+    class StudioLogic
+    {
+    }
+}
